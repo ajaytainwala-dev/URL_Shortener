3486c892959a6fac709cd98b2443e89ede2aab47
Ajay Tainwala <ajaytainwala@gmail.com>
2025-02-27 22:25:26 +0530

    Update

--- ai.js
+++ ai.js
@@ -1,6 +1,8 @@
 // const { GoogleGenerativeAI } = require("@google/generative-ai");
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI("AIzaSyBVRCZKy7KjkqWWBtONjgzLDDbPmJCh9-o");
+import dotenv from "dotenv";
+dotenv.config();
+const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const prompt = "check https://www.gyan-academy.in/ is malicious or not and give me only yes or no";
